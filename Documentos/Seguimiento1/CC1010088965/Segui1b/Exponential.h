f23c7344092491fbf99e5895bd62e4eeac8b9680
Camilo <camilo.gomez@guane.com.co>
2022-01-14 10:35:11 -0500

    Terminada Segui1a, empezada Segui1b

---
+++ Documentos/Seguimiento1/CC1010088965/Segui1b/Exponential.h
@@ -0,0 +1,17 @@
+#include <string> 
+using std::string;
+
+class Exponential {
+    public:
+    Exponential( string ); 
+    void establecerNombreCurso( string ); 
+    string obtenerNombreCurso();
+    void mostrarMensaje(); 
+    void recibirCalificaciones();
+    void mostrarReporteCalificaciones(); // muestra un reporte con base en las calificaciones
+    int maximo( int, int, int ); // determina el máximo de 3 valores
+    
+    private:
+    string nombreCurso;
+    int calificacionMaxima;
+};
