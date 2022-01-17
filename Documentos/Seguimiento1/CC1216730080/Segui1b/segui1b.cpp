243df6fa034db670bc8c2325a059deae4facf8dc
cdavidgutierrez <36546273+cdavidgutierrez@users.noreply.github.com>
2022-01-17 16:37:20 -0500

    Seguimiento1

---
+++ Documentos/Seguimiento1/CC1216730080/Segui1b/segui1b.cpp
@@ -0,0 +1,63 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+#include <unordered_map>
+#include <math.h>
+
+using namespace std;
+
+class Exponencial{
+    private:
+        // Variables privadas el valor de "x" y "n"la presicion.
+        float x;
+        int n;
+        
+        // Función que calcula el factorial.
+        unsigned long int Factorial(int n){
+            if (n<0){
+                return 0;
+            }
+            if (n==0 || n==1){
+                return 1;
+            } else {
+                return n*Factorial(n-1);
+            }
+        }
+    public:
+        // Definición del constructor de la clase.
+        Exponencial(float,int);
+        
+        // Función que calcula el valor de la exponencial.
+        float Exponential(float x, int n){
+            float etox=0;
+            for (int i=0; i<n+1; i++){
+                etox+=pow(x,i)/Factorial(i);
+            }
+            return etox;
+        }
+        
+        // Función que imprime el valor de exp(x) a orden n.
+        void printExp(){
+            cout << "El valor de exp(" << x << ") hasta orden " << n << " es " << Exponential(x,n);
+        }
+};
+
+// Cuerpo del constructor de manera explícita.
+Exponencial::Exponencial(float xx,int nn){
+    x=xx;
+    n=nn;
+}
+
+int main()
+{
+    Exponencial example1(1,8);
+    example1.printExp();
+
+    return 0;
+}
