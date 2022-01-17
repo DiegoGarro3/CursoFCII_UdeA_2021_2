bde6ae1a154c28de37f968c481b061ca289ad6fd
jmejiagu <jhovanny.andres.mejia.guisao@cern.ch>
2022-01-17 14:11:07 -0500

    clase6

---
+++ Documentos/clase6/example6_modificarA_RangeBase.cpp
@@ -0,0 +1,27 @@
+// Using range-based for 
+#include <iostream>
+#include <array>
+using namespace std;
+
+int main() {
+   array<int, 5> items{1, 2, 3, 4, 5};
+
+   // mostrar elementos antes de la modificacion 
+   cout << "items before modification: ";
+   for (int item : items) {
+      cout << item << " ";
+   }
+
+   //
+   for (int& itemRef : items) {
+      itemRef *= 2;
+   }
+
+   // mostrar elementos despues de la modificacion
+   cout << "\nitems after modification: ";
+   for (int item : items) {
+      cout << item << " ";
+   }
+
+   cout << endl;
+}
