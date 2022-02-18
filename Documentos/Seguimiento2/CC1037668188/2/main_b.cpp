aa7359ba283358a8c3722e2088972839a19b0586
Kevin <kevin.cortes@udea.edu.co>
2022-02-18 14:42:04 -0500

    Act

--- Documentos/Seguimiento2/CC1037668188/2/main_b.cpp
+++ Documentos/Seguimiento2/CC1037668188/2/main_b.cpp
@@ -17,29 +17,55 @@ int main()
     cout<<"Archivo leido: ConvertirNum.dat"<<endl;
     cout<<endl;
 
+    ofstream Archivo_salida( "Archivo_salida.dat" );
+  
+  	 
+  
+  	if(Archivo_salida.fail()){
+		cout<<"No se pudo crear el archivo";
+		exit(1);
+  	}
+
+
+
     int numero;
     string sis1;
     string sis2;
     cout<< "Número     Sist. Num. 1     Sist. Num. 2      Numero " << endl;
     cout<< "------     ------------     ------------      ------"<<endl;
+    
+
     while ( archivo >> numero >> sis1 >> sis2 )
     {
         cout<<left<<setw(13)<<numero<<setw(18)<< sis1<<setw(15)<< sis2;
         if(sis1=="decimal")
         {
-            if(sis2=="binario")
-            cout<< Conver.DecimalToBinario(numero);
+            if(sis2=="binario"){
+            cout<<Conver.DecimalToBinario(numero);
+	        Archivo_salida<<left<<setw(13)<<numero<<setw(13)<<sis1<<setw(13)<<sis2<<setw(13)<<Conver.DecimalToBinario(numero)<<endl;
+            };
             if(sis2=="Hex")
-            cout<< Conver.DecimalToHexadecimal(numero);
+            {
+            cout<<Conver.DecimalToHexadecimal(numero);
+            Archivo_salida<<left<<setw(13)<<numero<<setw(13)<<sis1<<setw(13)<<sis2<<setw(13)<<Conver.DecimalToHexadecimal(numero)<<endl;
+            }
         }
         if(sis1=="binario")
         {
             string num=to_string(numero);
             if(sis2=="decimal")
+            {
             cout<< Conver.BinarioToDecimal(num);
+            Archivo_salida<<left<<setw(13)<<numero<<setw(13)<<sis1<<setw(13)<<sis2<<setw(13)<<Conver.BinarioToDecimal(num)<<endl;
+            }
             if(sis2=="Hex")
+            {
             cout<< Conver.BinarioToHexadecimal(num);
+            Archivo_salida<<left<<setw(13)<<numero<<setw(13)<<sis1<<setw(13)<<sis2<<setw(13)<<Conver.BinarioToHexadecimal(num)<<endl;
+            }
         };
+
+
 
         /*   
         if(sis1=="Hex")
@@ -51,6 +77,8 @@ int main()
             cout<< Conver.HexadecimalToBinario(num);
         }; */
         cout<<endl;
+        
+    
 
 
     };
