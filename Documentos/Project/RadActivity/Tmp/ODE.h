681f66cbb689b012682fb24945999f82d5a2c137
jmejiagu <jhovanny.andres.mejia.guisao@cern.ch>
2022-04-08 14:10:50 -0500

    the last commit}

--- Documentos/Project/RadActivity/Tmp/ODE.h
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <iomanip>
-#include <valarray>
-using namespace std;
-
-void RK4step( double, double &, valarray<double> & );
-valarray<double> F( double , valarray<double> );
-void Solution(int, double, double, double, double);
